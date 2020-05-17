8de56b94acb423829d77bb8ec845f1bb16e9fc47
Shajal Kumer <shajalkumer@gmail.com>
2020-05-18 00:45:52 +0600

    Updates

---
+++ precache-manifest.dce2f4f0fe19b28e4329cf354bf9ad73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b1c2947aeaceabecd55e3fe4bf7486",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "2ea7dda65b25e8775e39",
+    "url": "/image-search/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1070904d41ad13cda443",
+    "url": "/image-search/static/css/main.d6911745.chunk.css"
+  },
+  {
+    "revision": "2ea7dda65b25e8775e39",
+    "url": "/image-search/static/js/2.66a8dfa7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-search/static/js/2.66a8dfa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1070904d41ad13cda443",
+    "url": "/image-search/static/js/main.ec59901a.chunk.js"
+  },
+  {
+    "revision": "79db83570f70510de54f",
+    "url": "/image-search/static/js/runtime-main.5ecf719d.js"
+  }
+]);
